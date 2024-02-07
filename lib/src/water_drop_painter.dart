47c5cf81eaf8977281dac74a34351b4d13a2c244
Bashar <bashar.ameen.ai@gmail.com>
2024-02-07 13:08:31 +0300

    f

--- lib/src/water_drop_painter.dart
+++ lib/src/water_drop_painter.dart
@@ -10,32 +10,33 @@ class WaterDropPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     Path path = Path();
 
-    double widthFactor = 1;
+    double widthFactor = textDirection == TextDirection.rtl ? -1 : 1;
+    double xOffset = textDirection == TextDirection.rtl ? size.width : 0;
 
     path.cubicTo(
-      size.width * 0.239841 * widthFactor,
+      xOffset + size.width * 0.239841 * widthFactor,
       size.height * 0.06489535,
-      size.width * 0.285956 * widthFactor,
+      xOffset + size.width * 0.285956 * widthFactor,
       size.height * 0.4886860,
-      size.width * 0.42016 * widthFactor,
+      xOffset + size.width * 0.42016 * widthFactor,
       size.height * 0.8271512,
     );
     path.cubicTo(
-      size.width * 0.467771 * widthFactor,
+      xOffset + size.width * 0.467771 * widthFactor,
       size.height * 0.9466628,
-      size.width * 0.530574 * widthFactor,
+      xOffset + size.width * 0.530574 * widthFactor,
       size.height * 0.9472209,
-      size.width * 0.578344 * widthFactor,
+      xOffset + size.width * 0.578344 * widthFactor,
       size.height * 0.8285814,
     );
     path.cubicTo(
-        size.width * 0.7185669 * widthFactor,
+        xOffset + size.width * 0.7185669 * widthFactor,
         size.height * 0.4786744,
-        size.width * 0.757325 * widthFactor,
+        xOffset + size.width * 0.757325 * widthFactor,
         size.height * 0.06629070,
-        size.width * 0.999682 * widthFactor,
+        xOffset + size.width * 0.999682 * widthFactor,
         0);
-    path.lineTo(0, 0);
+    path.lineTo(xOffset, 0);
     path.close();
 
     Paint fillColor = Paint()..style = PaintingStyle.fill;
